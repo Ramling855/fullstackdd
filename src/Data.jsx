c983c4d3ba4e5d73cbca9e862f69f0f03934f084
Ramling855 <99536932+Ramling855@users.noreply.github.com>
2022-06-20 09:41:33 +0530

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,76 +2,51 @@ import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 const Data = () => {
   const [getdata, setGetdata] = useState([]);
 //   const [start, setStart] = useState();
   useEffect(() => {
     axios
-      .get("https://ramlingsurvase.herokuapp.com/data")
-      .then((all) => setGetdata(all.data))
+      .get("https://thawing-reef-81365.herokuapp.com/form/getall")
+      .then((all) => setGetdata(all.data.data))
       .catch(() => console.log("errr"));
   });
-  const click1 = (ele) => {
-    let {  name, price, quantity, brand } = ele;
-    localStorage.setItem("ID", ele._id);
-    localStorage.setItem("Name", name);
-    localStorage.setItem("Price", price);
-    localStorage.setItem("Quantity", quantity);
-    localStorage.setItem("Brand", brand);
-    // console.log(ele._id);
-    // axios
-    //   .delete(`http://localhost:5000/auth/update`, { name: ele })
-    //   .then((all) => console.log("data"))
-    //   .catch(() => console.log("errr"));
-  };
 
-  const click = (ele) => {
-    console.log(ele);
-    axios
-      .delete(`https://ramlingsurvase.herokuapp.com/delete/${ele._id}`)
-      .then((all) => console.log("datadelete"))
-      .catch(() => console.log("errr"));
-  };
+  
 
   return (
     <>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Sr.No</th>
-            <th scope="col">name</th>
-            <th scope="col">brand</th>
-            <th scope="col">quantity</th>
-            <th scope="col">price</th>
-            <th scope="col">delete</th>
-            <th scope="col">update</th>
-          </tr>
-        </thead>
-        <tbody>
           {getdata.map((ele, i) => {
             return (
               <>
-                <tr>
-                  <th scope="row">{i + 1}</th>
-                  <td>{ele.name}</td>
-                  <td>{ele.brand}</td>
-                  <td>{ele.quantity}</td>
-                  <td>{ele.price}</td>
-                  <td>
-                    <button onClick={() => click(ele)}>delete</button>
-                  </td>
-                  <td>
-                    <button onClick={() => click1(ele)}>
-                      <Link to="/login">update</Link>
-                    </button>
-                  </td>
-                </tr>
+
+<div key={i} class="card mb-3" style={{width: "100%",height:"1000px"}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={ele.photo} className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Name :{ele.name}</h5>
+        <p className="card-text">address :{ele.address}</p>
+        <p className="card-text"><small className="text-muted">email: {ele.email}</small></p>
+        <p className="card-text"><small className="text-muted">Mo. No.:{ele.phone}</small></p>
+        <p className="card-text"><small className="text-muted">educational qualification.:{ele.education}</small></p>
+        <p className="card-text"><small className="text-muted">job designation.:{ele.job}</small></p>
+        {/* <p className="card-text"><small className="text-muted">job Location.:{ele.location}</small></p> */}
+        <p className="card-text"><small className="text-muted">monthly salary:{ele.salary}</small></p>
+
+
+      </div>
+    </div>
+  </div>
+  
+</div>
+
               </>
             );
           })}
-        </tbody>
-      </table>
+        
     </>
   );
 };
